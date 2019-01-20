522ab3408ed589369d9a07d3bfede658fdc1ef22
ggolikov <gr.golikov@gmail.com>
2019-01-20 23:56:15 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -1,69 +1,68 @@
-import '../../src/L.Rain';
-import { points } from './points';
+import '../../src/L.Snow';
+// import { points } from './points';
 
 $('#colorpicker').colorpicker();
 
-var osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+var osm = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }),
     center = [48, 10],
-    lmap = new L.Map('map', {layers: [osm], center, zoom: 5, maxZoom: 22, zoomAnimation: true}),
+    lmap = new L.Map('map', {layers: [osm], center, zoom: 4, maxZoom: 22, zoomAnimation: true}),
 
     root                  = document.querySelector('#content'),
     colorpickerController = document.querySelector('#colorpicker input'),
-    angleController       = document.querySelector('.angle-controller'),
-    widthController       = document.querySelector('.width-controller'),
-    spacingController     = document.querySelector('.spacing-controller'),
-    lengthController      = document.querySelector('.length-controller'),
-    intervalController    = document.querySelector('.interval-controller'),
     speedController       = document.querySelector('.speed-controller'),
+    layersCountController     = document.querySelector('.layersCount-controller'),
+    densityController     = document.querySelector('.density-controller'),
+    sizeController        = document.querySelector('.size-controller'),
 
     options = {
-        angle:      +angleController.value,          // deg
-        width:      +widthController.value,          // px
-        spacing:    +spacingController.value,      // px
-        length:     +lengthController.value,        // px
-        interval:   +intervalController.value,    // px
-        speed:      +speedController.value,          // times
-        color:      rgb2hex(colorpickerController.value)
+        speed:          +speedController.value,          // times
+        layersCount:    +layersCountController.value,          // times
+        density:        +densityController.value,          // times
+        size:           +sizeController.value,          // times
+        color:          rgb2hex(colorpickerController.value),
+        opacity: 0.5
     },
-    rain = L.rain(points, options).addTo(lmap);
-
-angleController.addEventListener('change', function (e) {
-    var angle = Number(e.target.value);
-    rain.setAngle(angle);
-});
-
-widthController.addEventListener('change', function (e) {
-    var width = Number(e.target.value);
-    rain.setWidth(width);
-});
-
-spacingController.addEventListener('change', function (e) {
-    var spacing = Number(e.target.value);
-    rain.setSpacing(spacing);
-});
-
-lengthController.addEventListener('change', function (e) {
-    var length = Number(e.target.value);
-    rain.setLength(length);
-});
-
-intervalController.addEventListener('change', function (e) {
-    var interval = Number(e.target.value);
-    rain.setInterval(interval);
-});
+    points = [
+        [
+            [35, -5],
+            [60, -5],
+            [60, 25],
+            [35, 25],
+            [35, -5]
+        ]
+    ],
+    snow = L.snow(points, options).addTo(lmap);
+    window.snow = snow;
 
 speedController.addEventListener('change', function (e) {
     var speed = Number(e.target.value);
-    rain.setSpeed(speed);
+    snow.setSpeed(speed);
+});
+
+layersCountController.addEventListener('change', function (e) {
+    var layersCount = Number(e.target.value);
+    snow.setLayersCount(layersCount);
+});
+
+densityController.addEventListener('change', function (e) {
+    var density = Number(e.target.value);
+    snow.setDensity(density);
+});
+
+sizeController.addEventListener('change', function (e) {
+    var size = Number(e.target.value);
+    snow.setSize(size);
 });
 
 $('#colorpicker').on('colorpickerChange', e => {
-    var color = e.color.toHexString();
+    var color = e.color.toHexString(),
+        opacity = e.color.alpha;
 
-    rain.setColor(color);
+    snow.setColor(color);
+    snow.setOpacity(opacity);
 });
 
 function rgb2hex(rgb) {
